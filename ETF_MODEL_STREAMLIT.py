7200f59f8cc67adabd404a6c02d019e12e0a1583
jprasham <119007126+jprasham@users.noreply.github.com>
2022-11-30 16:10:53 +0530

    Created using Colaboratory

--- ETF_MODEL_STREAMLIT.py
+++ ETF_MODEL_STREAMLIT.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyMLo+AYXF1QvJ7TXnSdyVxd",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyMLo+AYXF1QvJ7TXnSdyVxd"
     },
     "kernelspec": {
       "name": "python3",
@@ -16,16 +15,6 @@
     }
   },
   "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/jprasham/ETF_STREAMLIT/blob/main/ETF_MODEL_STREAMLIT.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
     {
       "cell_type": "code",
       "execution_count": 1,
