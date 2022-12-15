92f54c26bf7d4d48f7b902942aab6f2067adb7f3
jprasham <119007126+jprasham@users.noreply.github.com>
2022-12-16 01:32:42 +0530

    Update ETF_MODEL_STREAMLIT.py

--- ETF_MODEL_STREAMLIT.py
+++ ETF_MODEL_STREAMLIT.py
@@ -77,11 +77,11 @@ with st.sidebar:
   category = st.multiselect('Category:',category['category'],default= 'Equities')
   etf = st.multiselect('ETF Tickers:', ETFs['Ticker'],default= 'SPY')
 
-  if category  == [] AND etf == []:
+  if (category  == []) AND (etf == []):
     d_etf = ETFs.loc[ETFs['Ticker'] == 'SPY']
-  elif category != [] AND etf ==[]:
+  elif (category != []) AND (etf ==[]):
     d_etf = ETFs.loc[ETFs['Category'].isin(category)]
-  elif category == [] AND etf != []:
+  elif (category == []) AND (etf != []):
     d_etf = ETFs.loc[ETFs['Ticker'].isin(etf)] 
   else:  
     d_etf = ETFs.loc[(ETFs['Ticker'].isin(etf)) & (ETFs['Category'].isin(category)) ]                 
